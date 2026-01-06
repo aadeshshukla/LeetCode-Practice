3d588f3dac03a1f9038fca4b152f8b7026f33d9b
Aadesh Shukla <aadeshshukla470@gmail.com>
2026-01-06 23:28:54 +0530

    tuesday sucks

---
+++ tuesday.py
@@ -0,0 +1,8 @@
+#NEVER RUN THIS CODE , and be careful while using loops 
+#nothing learned today just got stuck with update and this infinte loop  problem , 
+
+# lst=[1,2,23,3,4,4,5,[1,2,3,3,4],[22,2,2,2]]
+# for i in lst:
+#     if type(i)==list:
+#         lst.append(i)
+# print(lst)        
