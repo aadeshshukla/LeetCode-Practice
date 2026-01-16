371a23651cd00cf1b0db127098f5d28c471239df
Aadesh Shukla <aadeshshukla470@gmail.com>
2026-01-16 10:10:41 +0530

    improving

---
+++ friday1.py
@@ -0,0 +1,25 @@
+#the fizzbuzz
+# for i in range(1,51):
+#     if i%3==0 and i%5==0:print("FizzBuzz")
+#     elif i%3==0:print("Fizz")
+#     elif i%5==0:print("Buzz")
+#     else:print(i)
+# _________________________________________________-
+# rules={3:"fizz",5:"buzz",7:"zap"}
+# for i in range(1,51):
+#     output=" "
+#     for j in rules:
+#         if i%j==0:
+#             output+=rules[j]
+#     if output==" ":print(i)
+#     else:print(output)        
+# ___________________________________________________
+def myfunction(s):
+    s=s.lower().replace(" ","")
+    is_pal=s==s[::-1]
+    counts={}
+    for char in s:
+        counts[char]=counts.get(char,0)+1
+    return is_pal,counts    
+result=myfunction("madam")
+print(result)
