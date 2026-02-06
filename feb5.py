95320473f3a04f559d22667427f94f58f03d21f8
Aadesh Shukla <aadeshshukla470@gmail.com>
2026-02-06 19:57:21 +0530

    workimg

--- feb5.py
+++ feb5.py
@@ -19,10 +19,49 @@
 #     stars='*'*i
 #     print(spaces+stars)
 # pattern 3
-rows=5
-for i in range(rows+1,0,-1) :
-    spaces=" "*i
-    stars='*'*rows
-    rows=rows-1
-    print(spaces+stars)
+# *****
+#  ****
+#   ***
+#    **
+#     *
+# rows=5
+# for i in range(1,rows+1) :
+#     spaces=" "*(i-1)
+#     stars='*'*rows
+#     rows-=1
+#     print(spaces+stars)
+# pattern 4
+#     *
+#    ***
+#   *****
+#  *******
+# rows=4
+# for i in range(1,rows+1) :
+#     spaces=" "*(rows-i)
+#     stars='*'*(2*i-1)
+#     print(spaces+stars)
+
+# pattern 5
+#     *
+#    **
+#   ***
+#  ****
+# *****
+# rows=5
+# for i in range(1,rows+1) :
+#     spaces=" "*(rows-i)
+#     stars='*'*i
+#     print(spaces+stars)
+
+# combine pattern 1 and 5 so we can print them side by side
+# rows=5
+# for i in range(1,rows+1) :
+#     spaces=" "*(rows-i)
+#     stars='*'*i
+#     print(spaces+stars+"  "+stars)
+
+
+
+
+
 
