41253384f4a3f08f8b755a1302dde819e5b66c7f
aadeshshukla <aadeshshukla470@gmail.com>
2026-02-15 20:56:15 +0530

    timeless

--- feb13.py
+++ feb13.py
@@ -43,3 +43,36 @@ def longest_substring_k_distinct(s, k):
     return max_length
 s = "eceba"
 print(longest_substring_k_distinct(s, 2))
+
+# sliding window pattern 2
+# 1. find the smallest window in a string containing all characters of another string
+def min_window(s, t):
+    from collections import Counter
+    t_count = Counter(t)
+    window_count = {}
+    have, need = 0, len(t_count)
+    left = 0
+    min_length = float('inf')
+    min_window_start = 0
+
+    for right in range(len(s)):
+        char = s[right]
+        window_count[char] = window_count.get(char, 0) + 1
+
+        if char in t_count and window_count[char] == t_count[char]:
+            have += 1
+
+        while have == need:
+            if (right - left + 1) < min_length:
+                min_length = right - left + 1
+                min_window_start = left
+
+            window_count[s[left]] -= 1
+            if s[left] in t_count and window_count[s[left]] < t_count[s[left]]:
+                have -= 1
+            left += 1
+
+    return s[min_window_start:min_window_start + min_length] if min_length != float('inf') else ""
+s = "ADOBECODEBANC"
+t = "ABC"
+print(min_window(s, t))
