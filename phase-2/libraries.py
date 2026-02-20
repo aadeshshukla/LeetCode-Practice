7e528bcf58c2ed99b089f67b7f333d7ce9d11d57
aadeshshukla <aadeshshukla470@gmail.com>
2026-02-20 19:40:32 +0530

    libraries

---
+++ phase-2/libraries.py
@@ -0,0 +1,18 @@
+# lets explore libraries in python 
+# maths 
+import math as m 
+print(m.sqrt(16)) # square root
+print(m.pow(2,3)) # power
+# random
+import random as r
+print(r.randint(1,10)) # random integer between 1 and 10
+print(r.random()) # random float between 0 and 1
+# datetime
+import datetime as dt
+print(dt.datetime.now()) # current date and time
+print(dt.date.today()) # current date
+# os
+import os
+print(os.getcwd()) # current working directory
+print(os.listdir()) # list of files in current directory
+
