dbe082d82691a57df1a6783ebc8c11f79ce354cf
aadeshshukla <aadeshshukla470@gmail.com>
2026-03-05 20:59:44 +0530

    semi

---
+++ mar5.py
@@ -0,0 +1,11 @@
+# python function.py
+def greet(name):
+    return f"Hello, {name}!"
+name = "Alice"
+message = greet(name)
+print(message)
+
+# lambda function
+square = lambda x: x ** 2
+print(square(5))
+
