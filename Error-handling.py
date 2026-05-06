12bb220c3400692739d1d4efcf76c92b1328d72a
aadeshshukla <aadeshshukla470@gmail.com>
2026-05-06 21:32:05 +0530

    offffff

--- Error-handling.py
+++ Error-handling.py
@@ -27,3 +27,18 @@ try:
     print(validate_password(password))
 except ValueError as e:
     print(f"Error: {e}")
+
+
+def leap(year):
+    leap=False
+    if year%400==0:
+        leap=True
+    elif year%100==0:
+        leap=False
+    elif  year%4==0:
+        leap=True
+    else:
+        leap=False
+    return leap
+    
+print(leap(2100))
