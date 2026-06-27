cb3d94b89750a9e881f7295b8e342aaa09b7a650
aadeshshukla <aadeshshukla470@gmail.com>
2026-06-27 20:38:53 +0530

    class

---
+++ class.py
@@ -0,0 +1,26 @@
+# class student:
+#     def __init__(self,name=None,course=None):
+#         self.name=name
+#         self.course=course
+#     def show(self):
+#         print(f"{self.name} is a student of {self.course}")
+#     def update(self,newname=None,newcourse=None):
+#         self.name=newname
+#         self.course=newcourse
+#     def add_more_details(self,age,city):
+#         self.age=age
+#         self.city=city
+
+# s1=student()
+# s1.name="Aadesh Shukla"
+# s1.course="Cse"
+# s1.show()
+# s2=student("Vaishu","cse")
+# s2.show()
+# s2.update("mausam","AIML")
+# s2.show()
+# s2.show()
+# s1.add_more_details(20,"Lucknow")
+# s1.show()
+
+
