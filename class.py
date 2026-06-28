78c8536337126870f830e7476e81408a8d2f3e45
aadeshshukla <aadeshshukla470@gmail.com>
2026-06-28 18:48:43 +0530

    class

--- class.py
+++ class.py
@@ -23,4 +23,49 @@
 # s1.add_more_details(20,"Lucknow")
 # s1.show()
 
+# 
+class player:
+    def __init__(self,name=None,team=None):
+        self.name=name
+        self.team=team
+    def show(self):
+        print(f"{self.name} is a player of {self.team}")
+    def update(self,newname=None,newteam=None):
+        self.name=newname
+        self.team=newteam
+    def add_more_details(self,age,city):
+        self.age=age
+        self.city=city
 
+p1=player()
+p1.name="Virat Kohli"   
+p1.team="India"
+p1.show()
+p2=player("Rohit","India")
+p2.show()
+p2.update("Rohit Sharma","India")
+p2.show()
+
+# what more we can do with class and object
+# inheritance
+class cricket(player):
+    def __init__(self,name=None,team=None,role=None):
+        super().__init__(name,team)
+        self.role=role
+    def show(self):
+        print(f"{self.name} is a {self.role} of {self.team}")   
+        
+c1=cricket()
+c1.name="Sachin"
+c1.team="India"
+c1.role="Batsman"
+c1.show()
+c2=cricket("Rohit","India","Batsman")
+c2.show()
+
+# error:Traceback (most recent call last):
+#   File "D:\Users\91939\Desktop\LeetCode-practice\class.py", line 65, in <module>
+#     c2.update("RohitSharma","India","Batsman")
+# TypeError: player.update() takes from 1 to 3 positional arguments but 4 were given
+c2.update("RohitSharma","India")
+c2.show()
